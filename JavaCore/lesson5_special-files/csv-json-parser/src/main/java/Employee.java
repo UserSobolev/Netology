3fb8a1f86458656f80b31eadfbe947e571ba0d24
Anton Sobolev <anton.sobolev.92@gmail.com>
2022-04-11 14:01:59 +0300

    lesson5 special files, xml to json dosen't work

--- JavaCore/lesson5_special-files/csv-json-parser/src/main/java/Employee.java
+++ JavaCore/lesson5_special-files/csv-json-parser/src/main/java/Employee.java
@@ -1,15 +1,15 @@
 public class Employee {
-    public long id;
+    public String  id;
     public String firstName;
     public String lastName;
     public String country;
-    public int age;
+    public String  age;
 
     public Employee() {
         // Пустой конструктор
     }
 
-    public Employee(long id, String firstName, String lastName, String country, int age) {
+    public Employee(String id, String firstName, String lastName, String country, String  age) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
--- JavaCore/lesson5_special-files/csv-json-parser/src/main/java/Main.java
+++ JavaCore/lesson5_special-files/csv-json-parser/src/main/java/Main.java
@@ -1,19 +1,22 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonToken;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import com.opencsv.exceptions.CsvValidationException;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -21,13 +24,22 @@ public class Main {
         //Создаем массив строк с информацией о преднозначении колонок в CSV файле
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         //Определяем имя для считываемого файла
-        String fileName = "data.csv";
-        //Получаем список сотрудников из файла csv
-        List<Employee> list = parseCSV(columnMapping, fileName);
+        String fileNameCSV = "data.csv";
+        //Получаем список объектов Employee из файла csv
+        List<Employee> listFromCsv = parseCSV(columnMapping, fileNameCSV);
         //Преобразуем список сотрудников в строку формата JSON
-        String json = listToJson(list);
+        String json1 = listToJson(listFromCsv);
         //Записываем строку JSON в файл JSON
-        writeString(json);
+        writeString(json1, "data.json");
+
+        //Определяем имя для считываемого файла
+        String fileNameXML = "data.xml";
+        //Получаем список объектов Employee из файла xml
+        List<Employee> listFromXML = parseXML(fileNameXML);
+        //Преобразуем список сотрудников в строку формата JSON
+        String json2 = listToJson(listFromXML);
+        //Записываем строку JSON в файл JSON
+        writeString(json2, "data2.json");
     }
     //Метод принимает на вход массив с преднозначениемколонок в файле CSV + сам файл CSV,
     //парсит файл и создает список объектов Employee
@@ -52,17 +64,48 @@ public class Main {
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
         Type listType = new TypeToken<List<Employee>>() {}.getType();
-        String json = gson.toJson(list, listType);
-        return json;
+        return gson.toJson(list, listType);
     }
     //Метод принимает на вход строку формата JSON и пишет ее в файл "data.json"
-    public static void writeString(String json) {
+    public static void writeString(String json, String namaFile) {
         try (FileWriter file = new
-                FileWriter("data.json")) {
+                FileWriter(namaFile)) {
             file.write(json);
             file.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public static List<Employee> parseXML(String fileName) {
+        List<Employee> list = new ArrayList<>();
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new File(fileName));
+            //Node roo = doc.getDocumentElement();
+            NodeList nodeList = doc.getElementsByTagName("employee");
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                System.out.println(node.getChildNodes());
+                Element element = (Element) node;
+                System.out.println(element.getNodeName());
+            }
+//            for (int i = 0; i < nodeList.getLength(); i++) {
+//                Node node = nodeList.item(i);
+//                if (Node.ELEMENT_NODE == node.getNodeType()) {
+//                    Element element = (Element) node;
+//                    Employee temp = new Employee(element.getAttribute("id"), element.getAttribute("firstName"),
+//                            element.getAttribute("lastName"), element.getAttribute("country"),
+//                            element.getAttribute("age"));
+//                    list.add(temp);
+//                }
+//            }
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
+
+
