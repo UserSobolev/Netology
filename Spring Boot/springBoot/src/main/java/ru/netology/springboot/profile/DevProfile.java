919ac615003093d509dac52ef2425fbc81bac0b7
Anton Sobolev <anton.sobolev.92@gmail.com>
2022-10-15 21:20:58 +0300

    Spring_SpringBoot_lesson1_SpringBootConditional_update

--- Spring Boot/springBoot/src/main/java/ru/netology/springboot/profile/DevProfile.java
+++
@@ -1,8 +0,0 @@
-package ru.netology.springboot.profile;
-
-public class DevProfile implements SystemProfile {
-    @Override
-    public String getProfile() {
-        return "Current profile is dev";
-    }
-}
--- Spring Boot/springBoot/src/main/java/ru/netology/springboot/profile/SystemProfile.java
+++
@@ -1,5 +0,0 @@
-package ru.netology.springboot.profile;
-
-public interface SystemProfile {
-    String getProfile();
-}
