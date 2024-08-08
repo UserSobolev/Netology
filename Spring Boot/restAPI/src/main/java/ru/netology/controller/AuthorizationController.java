9f624c908e57c7d02399badc99835703091f2e9c
Anton Sobolev <anton.sobolev.92@gmail.com>
2024-08-08 13:10:43 +0300

    docker

--- Spring Boot/restAPI/src/main/java/ru/netology/controller/AuthorizationController.java
+++ Spring Boot/restAPI/src/main/java/ru/netology/controller/AuthorizationController.java
@@ -1,8 +1,6 @@
 package ru.netology.controller;
 
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.netology.enums.Authorities;
 import ru.netology.model.User;
